d0a4d604a47d5d96683f6b7f8fba73c42bbaa33a
lexx2406 <lexx2406@yandex.ru>
2023-06-16 01:18:37 +0400

    написаны тесты для ArrayTest

--- MovieQuizTests/ArrayTest.swift
+++ MovieQuizTests/ArrayTest.swift
@@ -5,4 +5,35 @@
 //  Created by Алексей Налимов on 16.06.2023.
 //
 
-import Foundation
+import XCTest
+@testable import MovieQuiz
+
+class ArrayTest: XCTestCase {
+    
+    func testGetValueInRange() throws {
+        
+        //Given
+        let array = [1, 2, 3, 4, 5]
+        
+        //When
+        let value = array[safe: 2]
+
+        //Then
+        XCTAssertNotNil(value)
+        XCTAssertEqual(value, 3)
+    }
+    
+    func testGetValueOutOfRange() {
+        
+        //Given
+        let array = [1, 2, 3, 4, 5]
+        
+        //When
+        let value = array[safe: 20]
+        
+        //Then
+        XCTAssertNil(value)
+        
+    }
+    
+}
