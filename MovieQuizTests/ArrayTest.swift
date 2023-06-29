cbdeaec4565ff24e0bd9bacd0ffaa27089e8a26d
lexx2406 <lexx2406@yandex.ru>
2023-06-30 00:28:41 +0400

    Удалены комменты в тестах

--- MovieQuizTests/ArrayTest.swift
+++ MovieQuizTests/ArrayTest.swift
@@ -12,26 +12,16 @@ class ArrayTest: XCTestCase {
     
     func testGetValueInRange() throws {
         
-        //Given
         let array = [1, 2, 3, 4, 5]
-        
-        //When
         let value = array[safe: 2]
-
-        //Then
         XCTAssertNotNil(value)
         XCTAssertEqual(value, 3)
     }
     
     func testGetValueOutOfRange() {
         
-        //Given
         let array = [1, 2, 3, 4, 5]
-        
-        //When
         let value = array[safe: 20]
-        
-        //Then
         XCTAssertNil(value)
         
     }
--- MovieQuizTests/MoviesLoaderTest.swift
+++ MovieQuizTests/MoviesLoaderTest.swift
@@ -10,16 +10,13 @@ import XCTest
 
 class MoviesLoaderTests: XCTestCase {
     func testSuccessLoading() throws {
-        
-        // Given
+
         let stubNetworkClient = StubNetworkClient(emulateError: false)
         let loader = MoviesLoader(networkClient: stubNetworkClient)
-        
-        // When
         let expectation = expectation(description: "Loading expectation")
         
         loader.loadMovies { result in
-            // Then
+
             switch result {
             case .success(let movies):
                 XCTAssertEqual(movies.items.count, 2)
@@ -33,15 +30,11 @@ class MoviesLoaderTests: XCTestCase {
     }
     func testFailureLoading() throws {
         
-        // Given
         let stubNetworkClient = StubNetworkClient(emulateError: true)
         let loader = MoviesLoader(networkClient: stubNetworkClient)
-        
-        // When
         let expectation = expectation(description: "Loading expectation")
         
         loader.loadMovies { result in
-            // Then
             switch result {
             case .failure(let error):
                 XCTAssertNotNil(error)
